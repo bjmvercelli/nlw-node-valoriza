f08049b1a7a4f7048313d2ed76229bd0a0cb3782
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-07-06 21:24:28 -0300

    Database, entidades e repositorio

---
+++ src/entities/User.ts
@@ -0,0 +1,32 @@
+import {Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    readonly id: string; //a alteração do valor não pode ser feita por outra classe
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {//verifica se o id não está preenchido (criação de novo usuario) ou se ja está (remoção ou atualização)
+            this.id = uuid();
+        }
+    }
+}
+
+export { User };
